80a845ecf451c416888f3a235f5e7c6266477cec
Darci Neto <darci.neto@hotmail.com>
2020-10-15 00:43:54 -0300

    Refactor basico da classe Validation, garantindo mesmas entradas e saidas atraves de testes unitarios

--- src/main/java/cc/code/Validation.java
+++ src/main/java/cc/code/Validation.java
@@ -5,35 +5,23 @@ import java.util.List;
 
 public class Validation {
 
-	protected List<String> validaPessoa(Pessoa pessoa) {
-		
-		List<String> constraints = new ArrayList<String>();
-		if (pessoa != null) {
-			if (pessoa.cpf != null) {
-				if (pessoa.name != null) {
-					if (pessoa.telefonesFixos != null) {
-						for (String telefone : pessoa.telefonesFixos) {
-							if (telefone != null) {
-								if (telefone.isEmpty()) {
-									constraints.add("Erro - Telefone inválido");
-									return constraints;
-								} else {
-									if(telefone.length() < 7){
-										constraints.add("Erro - Telefone inválido");
-										return constraints;
-									}
-								}
-								
-							} else {
-								constraints.add("Erro - Telefone inválido");
-								return constraints;
-							}
-						}
-					}
-				}
-			}
-		}
+    protected List<String> validaPessoa(Pessoa pessoa) {
+        List<String> constraints = new ArrayList<>();
+        if (pessoa == null || pessoa.cpf == null || pessoa.name == null || pessoa.telefonesFixos == null || pessoa.telefonesFixos.isEmpty()) {
+            return constraints;
+        }
+        if (validaTelefoneFixo(pessoa)) {
+            constraints.add("Erro - Telefone inválido");
+        }
+        return constraints;
+    }
 
-		return constraints;
-	}
+    private boolean validaTelefoneFixo(Pessoa pessoa) {
+        for (String telefone : pessoa.telefonesFixos) {
+            if (telefone == null || telefone.isEmpty() || telefone.length() < 7) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
